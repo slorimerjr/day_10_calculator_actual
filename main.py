76fe2d785bccab4ac1cd1d7fc69a6073fa66c477
Sam Lorimer <slorimerjr@gmail.com>
2022-04-13 18:20:09 +0000

    lesson complete

--- main.py
+++ main.py
@@ -76,6 +76,15 @@ num2 = int(input("What's the second number? \n"))
 
 calculation_function = operations[operation_symbol]
 #because we included returns in our functions, the 'calculation_function' will be replaced by them and act just like they would:
-answer = calculation_function(num1, num2)
+first_answer = calculation_function(num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+operation_symbol = input("Pick an operator from the line above: ")
+num3 = int(input("What's the second number? \n"))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(first_answer, num3)
+
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+
+
